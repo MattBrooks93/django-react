2074ee57c981893b3c4519c34e0d2c12cac230dd
Colby <colbyhepworth@gmail.com>
2023-08-03 15:45:13 -0400

    fixed url endpoints and imports

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,23 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from BusReservationSystem.views import customer_login, employee_login
+from django.urls import path
+from BusReservationSystem.views import customer_login, employee_login, employee_signup, customer_signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/customer', customer_login, name='customer_login'),
     path('api/login/employee', employee_login, name='employee_login'),
     path('api/register/employee', employee_signup, name='employee_signup'),
-    path('api/register/customer', customer_signup, name='customer_signup'),
-
-    path('api/trips', trips, name='trips'),
-    path('api/reservations', reservations, name='reservations'),
-    path('api/reservations/<int:reservation_id>', reservation, name='reservation'),
-    path('api/trips/<int:trip_id>', trip, name='trip'),
-    path('api/trips/<int:trip_id>/reservations', trip_reservations, name='trip_reservations'),
-    path('api/trips/<int:trip_id>/reservations/<int:reservation_id>', trip_reservation, name='trip_reservation'),
-    path('api/trips/<int:trip_id>/stations', trip_stations, name='trip_stations'),
-    path('api/trips/<int:trip_id>/stations/<int:station_id>', trip_station, name='trip_station'),
-    path('api/trips/<int:trip_id>/stations/<int:station_id>/reservations', trip_station_reservations)
+    path('api/register/customer', customer_signup, name='customer_signup')
 ]
+
+# path('api/trips', trips, name='trips'),
+# path('api/reservations', reservations, name='reservations'),
+# path('api/reservations/<int:reservation_id>', reservation, name='reservation'),
+# path('api/trips/<int:trip_id>', trip, name='trip'),
+# path('api/trips/<int:trip_id>/reservations', trip_reservations, name='trip_reservations'),
+# path('api/trips/<int:trip_id>/reservations/<int:reservation_id>', trip_reservation, name='trip_reservation'),
+# path('api/trips/<int:trip_id>/stations', trip_stations, name='trip_stations'),
+# path('api/trips/<int:trip_id>/stations/<int:station_id>', trip_station, name='trip_station'),
+# path('api/trips/<int:trip_id>/stations/<int:station_id>/reservations', trip_station_reservations)
+
