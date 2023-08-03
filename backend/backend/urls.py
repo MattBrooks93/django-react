60e5d5a86097076cf9e3709b88b8be8a32e33ac9
Colby <colbyhepworth@gmail.com>
2023-08-03 15:34:21 -0400

    added url endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,5 +22,17 @@ from BusReservationSystem.views import customer_login, employee_login
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/customer', customer_login, name='customer_login'),
-    path('api/login/employee', employee_login, name='employee_login')
+    path('api/login/employee', employee_login, name='employee_login'),
+    path('api/register/employee', employee_signup, name='employee_login'),
+    path('api/register/customer', customer_signup, name='customer_login'),
+
+    path('api/trips', trips, name='trips'),
+    path('api/reservations', reservations, name='reservations'),
+    path('api/reservations/<int:reservation_id>', reservation, name='reservation'),
+    path('api/trips/<int:trip_id>', trip, name='trip'),
+    path('api/trips/<int:trip_id>/reservations', trip_reservations, name='trip_reservations'),
+    path('api/trips/<int:trip_id>/reservations/<int:reservation_id>', trip_reservation, name='trip_reservation'),
+    path('api/trips/<int:trip_id>/stations', trip_stations, name='trip_stations'),
+    path('api/trips/<int:trip_id>/stations/<int:station_id>', trip_station, name='trip_station'),
+    path('api/trips/<int:trip_id>/stations/<int:station_id>/reservations', trip_station_reservations)
 ]
